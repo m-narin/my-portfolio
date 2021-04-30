0245292d68dabca2a746e56f4fc7595121fd5005
mando-narin <y2pgftpe@keio.jp>
2021-04-30 20:49:34 +0900

    ver2

--- main.js
+++ main.js
@@ -2,18 +2,18 @@ const downMessage1 = document.getElementById("down_message1")
 const downMessage2 = document.getElementById("down_message2")
 const downMessage3 = document.getElementById("down_message3")
 
-const downMessage = document.getElementById("ds_down_down")
+const topMessage = document.getElementById("top_message")
 
 downMessage1.onclick = function () {
-  downMessage.textContent = "1998年5月20日"
+  topMessage.textContent = "1998年5月20日"
 }
 
 downMessage2.onclick = function () {
-  downMessage.textContent = "慶應"
+  topMessage.textContent = "慶應"
 }
 
 downMessage3.onclick = function () {
-  downMessage.textContent = "ラーメン"
+  topMessage.textContent = "ラーメン"
 }
 
 
